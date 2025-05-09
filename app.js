2fde6c26389779eaa474ec41b1226d916b95a997
kbrsrkl1 <kubranur216@gmail.com>
2025-05-09 08:56:46 +0300

    commit

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-const navbarEl = document.querySelector(".navbar");
+const navbarEl = document.querySelector(".navbar")
 const bottomEl = document.querySelector(".bottom")
-
 
 console.log(bottomEl.offsetTop)
 
+console.log(navbarEl.offsetHeight)
 
 
-window.addEventListener("scroll", ()=> {
+window.addEventListener("scroll", () => {
     if(
         window.scrollY > bottomEl.offsetTop - navbarEl.offsetHeight - 50
     ){
-        navbarEl.classList.add("active");
-    } else {
+        navbarEl.classList.add("active")
+    }else(
         navbarEl.classList.remove("active")
-    }
-});
-
+    )
+})
